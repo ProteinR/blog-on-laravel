b5b39d7d5692fa98f6b34970a25f38555d2424fc
Dmitriy <proteinr@gmail.com>
2019-01-14 23:56:00 +0200

    Models, view added

--- app/Comment.php
+++ app/Comment.php
@@ -7,4 +7,31 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     //
+    public function post()
+    {
+        return $this->hasOne(Post::class);
+    }
+
+    public function author()
+    {
+        return $this->hasOne(User::class);
+    }
+
+    //Prove comment
+    public function allow(){
+        $this->status = 1;
+        $this->save();
+    }
+
+    public function disallow(){
+        $this->status = 0;
+        $this->save();
+    }
+
+    public function togleStatus(){
+        if($this->status = 0){
+            $this->allow();
+        }
+        $this->disAllow();
+    }
 }
--- app/User.php
+++ app/User.php
@@ -5,16 +5,16 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\Storage;
 
 class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    const IS_BANNED = 1;
+    const IS_ACTIVE = 0;
+
+
     protected $fillable = [
         'name', 'email', 'password',
     ];
@@ -27,4 +27,87 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function posts(){
+        return $this->hasMany(Post::class);
+    }
+
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
+
+    public static function add($fields) {
+        $user = new static();
+        $user->fill($fields);
+        $user->password->bcrypt($fields['password']);
+        $user->save();
+        return $user;
+    }
+
+    public function edit($fields){
+        $this->fill($fields);
+        $this->password->bcrypt($fields['password']);
+        $this->save();
+    }
+
+    public function remove(){
+        Storage::delete('uploads/' . $this->image);
+        $this->delete();
+    }
+
+    public function uploadAvatar($image){
+        if($image == null) { //если картинка не зашла - выйти
+            return;
+        }
+        Storage::delete('uploads/' . $this->image);
+        $filename = str_random(10) . '.' . $image->extension();
+        $image->saveAS('uploads', '$filename');
+        $this->image = $filename;
+        $this->save();
+    }
+
+    public function getAvatar(){
+        if ($this->image == null){
+            return '/img/no-user-image.png';
+        }
+        return '/uploads/' . $this->image;
+    }
+
+
+    //Admin privilege
+    public function makeAdmin(){
+        $this->is_admin = 1;
+        $this->save();
+    }
+
+    public function makeNormal(){
+        $this->is_admin = 0;
+        $this->save();
+    }
+
+    public function togleAdmin($value){
+        if($value == null){
+            return $this->makeNormal();
+        }
+        return $this->makeAdmin();
+    }
+
+    //BAN
+    public function ban(){
+        $this->status = User::IS_BANNED;
+        $this->save();
+    }
+
+    public function unban(){
+        $this->status = User::IS_ACTIVE;
+        $this->save();
+    }
+
+    public function togleBan($value){
+        if($value == null){
+            return $this->unban();
+        }
+        return $this->ban();
+    }
+
 }
